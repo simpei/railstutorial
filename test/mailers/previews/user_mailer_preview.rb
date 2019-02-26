7f40b2bb32a95cfdebd69d94d2aa230a391252a9
shimblog <info.shimblog@gmail.com>
2019-02-26 11:14:53 +0900

    Add password reset

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,7 +2,7 @@
 class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at
-  # http://192.168.33.10:3000/rails/mailers/user_mailer/account_activation
+  # http://localhost:3000/rails/mailers/user_mailer/account_activation
   def account_activation
     user = User.first
     user.activation_token = User.new_token
@@ -12,6 +12,8 @@ class UserMailerPreview < ActionMailer::Preview
   # Preview this email at
   # http://localhost:3000/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 end
